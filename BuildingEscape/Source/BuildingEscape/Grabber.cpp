5c7c99cb1e32ebceaf7139095583e096c8743c0d
seanmajorpayne <seanmajorpayne@gmail.com>
2021-06-02 10:49:17 +0900

    adding grabbing functionality

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -1,10 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
-#include "Grabber.h"
 #include "GameFramework/PlayerController.h"
 #include "Engine/World.h"
 #include "DrawDebugHelpers.h"
+#include "CollisionQueryParams.h"
+#include "GameFramework/Actor.h"
+#include "Grabber.h"
 
 #define OUT
 
@@ -15,8 +16,6 @@ UGrabber::UGrabber()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
 }
 
 
@@ -25,28 +24,71 @@ void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogTemp, Warning, TEXT("The grabber is active!"));
-	
+	FindPhysicsHandle();
+	SetUpInputComponent();
 }
 
+void UGrabber::FindPhysicsHandle()
+{
+	PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+	if (!PhysicsHandle)
+	{
+		UE_LOG(LogTemp, Error, TEXT("PhysicsHandle does not exist on %s"), *GetOwner()->GetName());
+	}
+}
+
+void UGrabber::SetUpInputComponent()
+{
+	Input = GetOwner()->FindComponentByClass<UInputComponent>();
+	Input->BindAction("Grab", IE_Pressed, this, &UGrabber::Grab);
+	Input->BindAction("Grab", IE_Released, this, &UGrabber::Release);
+}
+
+void UGrabber::Grab()
+{
+	FHitResult HitResult = GetFirstPhysicsBodyInReach();
+	UPrimitiveComponent* ComponentToGrab = HitResult.GetComponent();
+	if(HitResult.GetActor())
+	{
+		PhysicsHandle->GrabComponentAtLocation
+		(
+			ComponentToGrab,
+			NAME_None,
+			LineTraceEnd
+		);
+	}
+}
+
+void UGrabber::Release()
+{
+
+}
 
 // Called every frame
 void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	if (PhysicsHandle->GrabbedComponent)
+	{
+		SetLineTraceEnd();
+		PhysicsHandle->SetTargetLocation(LineTraceEnd);
+	}
+}
 
-	FVector PlayerViewLocation;
-	FRotator PlayerViewRotation;
+void UGrabber::SetLineTraceEnd()
+{
 	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
 		OUT PlayerViewLocation, OUT PlayerViewRotation
 	);
 
-	UE_LOG(LogTemp, Warning, TEXT("Location: %s"), *PlayerViewLocation.ToString());
-	UE_LOG(LogTemp, Warning, TEXT("Rotation: %s"), *PlayerViewRotation.ToString());
-
 	// Draw a line from player showing the reach.
 
-	FVector LineTraceEnd = PlayerViewLocation + PlayerViewRotation.Vector() * Reach;
+	LineTraceEnd = PlayerViewLocation + PlayerViewRotation.Vector() * Reach;
+}
+
+FHitResult UGrabber::GetFirstPhysicsBodyInReach()
+{
+	SetLineTraceEnd();
 	DrawDebugLine(
 		GetWorld(),
 		PlayerViewLocation,
@@ -60,7 +102,22 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 
 
 	// Ray-cast out to a certain distance
+	FCollisionQueryParams TraceParams(FName(TEXT("")), false, GetOwner());
+	FHitResult Hit;
+	GetWorld()->LineTraceSingleByObjectType(
+		OUT Hit,
+		PlayerViewLocation,
+		LineTraceEnd,
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		TraceParams
+	);
+	AActor* ActorHit = Hit.GetActor();
+	if (ActorHit)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s was hit!"), *ActorHit->GetName());
+	}
 
+	return Hit;
 	// See what it hits (can we pick up)
 }
 
--- BuildingEscape/Source/BuildingEscape/Grabber.h
+++ BuildingEscape/Source/BuildingEscape/Grabber.h
@@ -4,6 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "PhysicsEngine/PhysicsHandleComponent.h"
+#include "Components/InputComponent.h"
 #include "Grabber.generated.h"
 
 
@@ -16,16 +18,27 @@ public:
 	// Sets default values for this component's properties
 	UGrabber();
 
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-
 private:
-	float Reach = 100.f;
-		
-	
+	float Reach = 200.f;
+	UPhysicsHandleComponent* PhysicsHandle = nullptr;
+	UInputComponent* Input = nullptr;
+
+	void FindPhysicsHandle();
+	void SetUpInputComponent();
+	void Grab();
+	void Release();
+	void SetLineTraceEnd();
+
+	FHitResult GetFirstPhysicsBodyInReach();
+	FVector LineTraceEnd;
+	FVector PlayerViewLocation;
+	FRotator PlayerViewRotation;
 };
